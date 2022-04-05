a4cc6a46c0f56970d52d40d87cbb783acc725cf5
usirin01 <usirin2021@gmail.com>
2022-04-05 22:40:33 +0200

    backend was started

---
+++ Node_Npm_2/calculate.js
@@ -0,0 +1,8 @@
+let number1 = 5;
+let number2 = 10;
+
+const getName = () => {
+    return "Ahmet";
+}; 
+
+module.exports = {number1, number2};
---
+++ Node_Npm_2/index_2.js
@@ -0,0 +1,6 @@
+//const {number1, number2, getName} = require("calculate");
+import number3 from "./calculate";
+
+console.log(number3);
+// console.log(number2);
+// console.log(getName());
